d2764bba05075634a4a273d2237034af4863249c
Utkarsh-626744 <mishrashiv.sm786gmail.com>
2022-09-28 01:33:06 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,208 @@
+import React, { Component } from "react";
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import { apiGetAccountAssets } from "./apis";
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
+const INITIAL_STATE = {
+  connector: null,
+  fetching: false,
+  connected: false,
+  chainId: 1,
+  showModal: false,
+  pendingRequest: false,
+  uri: "",
+  accounts: [],
+  address: "",
+  result: null,
+  assets: [],
+  balance: "-",
+};
+
+export default class App extends Component {
+  state = {
+    ...INITIAL_STATE,
+  };
+
+  connect = async () => {
+    // bridge url
+    const bridge = "https://bridge.walletconnect.org";
+
+    // create new connector
+    const connector = new WalletConnect({ bridge, qrcodeModal: QRCodeModal });
+
+    await this.setState({ connector });
+
+    // check if already connected
+    if (!connector.connected) {
+      // create new session
+      await connector.createSession();
+    }
+
+    // subscribe to events
+    await this.subscribeToEvents();
+  };
+  subscribeToEvents = () => {
+    const { connector } = this.state;
+
+    if (!connector) {
+      return;
+    }
+
+    connector.on("session_update", async (error, payload) => {
+      console.log(`connector.on("session_update")`);
+
+      if (error) {
+        throw error;
+      }
+
+      const { chainId, accounts } = payload.params[0];
+      this.onSessionUpdate(accounts, chainId);
+    });
+
+    connector.on("connect", (error, payload) => {
+      console.log(`connector.on("connect")`, payload);
+
+      if (error) {
+        throw error;
+      }
+
+      this.onConnect(payload);
+    });
+
+    connector.on("disconnect", (error, payload) => {
+      console.log(`connector.on("disconnect")`);
+
+      if (error) {
+        throw error;
+      }
+
+      this.onDisconnect();
+    });
+
+    if (connector.connected) {
+      // console.log(connector);
+      const { chainId, accounts } = connector;
+      const address = accounts[0];
+      this.setState({
+        connected: true,
+        chainId,
+        accounts,
+        address,
+      });
+      this.onSessionUpdate(accounts, chainId);
+    }
+
+    this.setState({ connector });
+  };
+
+  killSession = async () => {
+    const { connector } = this.state;
+    if (connector) {
+      connector.killSession();
+    }
+    this.resetApp();
+  };
+
+  resetApp = async () => {
+    this.setState({ ...INITIAL_STATE });
+  };
+
+  onConnect = async (payload) => {
+    const { chainId, accounts } = payload.params[0];
+    const address = accounts[0];
+    this.setState(
+      {
+        connected: true,
+        chainId,
+        accounts,
+        address,
+      },
+      () => {
+        this.getAccountAssets();
+      }
+    );
+  };
+
+  onDisconnect = async () => {
+    this.resetApp();
+  };
+
+  onSessionUpdate = async (accounts, chainId) => {
+    const address = accounts[0];
+    this.setState({ chainId, accounts, address }, () => {
+      this.getAccountAssets();
+    });
+  };
+
+  getAccountAssets = async () => {
+    const { address, chainId } = this.state;
+    this.setState({ fetching: true });
+    try {
+      // get account balances
+      const assets = await apiGetAccountAssets(address, chainId);
+
+      this.setState({ fetching: false, address, assets });
+    } catch (error) {
+      console.error(error);
+      this.setState({ fetching: false });
+    }
+  };
+
+  onCryptoChange = (event) => {
+    this.setState({ ...this.state, balance: event.target.value });
+  };
+
+  render() {
+    const {
+      assets,
+      address,
+      connected,
+      fetching,
+      balance,
+    } = this.state;
+    console.log(assets);
+
+    return (
+      <>
+        <div className="container p-5">
+          <nav className="navbar navbar-expand-lg px-4">
+            <div className="container-fluid">
+              <span className="navbar-brand">
+                Crypto Stats
+              </span>
+              <div className="collapse navbar-collapse justify-content-end">
+                {connected ? <button onClick={this.killSession} className="btn btn-danger">
+                  disconnect
+                </button> : <button disabled={fetching} onClick={this.connect} className="btn btn-primary">connect</button>}
+              </div>
+            </div>
+          </nav>
+
+          {address && <div className="container p-5">
+            <div className="my-3">
+              Wallet Address <span className="text-primary">{address}</span>
+            </div>
+            <div className="mt-2">
+              <div className="row">
+                <div className="col-8">
+                  <select className="form-select" onChange={this.onCryptoChange}>
+                    <option value="-">Select crypto</option>
+                    {assets.map((asset) => (
+                      <option key={asset.name} value={asset.balance}>{asset.name}</option>
+                    ))}
+                  </select>
+                </div>
+                <div className="col-4">
+                  <h3>Balance: <span>{balance}</span></h3>
+                </div>
+              </div>
+            </div>
+          </div>}
+
+        </div>
+      </>
+
+    );
+  }
+}
---
+++ src/apis.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://ethereum-api.xyz",
+  timeout: 30000, // 30 secs
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export async function apiGetAccountAssets(address, chainId) {
+    // console.log(address, chainId);
+    const response = await api.get(`/account-assets?address=${address}&chainId=${chainId}`);
+    const {
+        result
+    } = response.data;
+    return result;
+}
